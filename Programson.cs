ce30921b63ebe895e62d8580131c353668c17647
Lamie2 <100703059+Lamie2@users.noreply.github.com>
2022-03-16 13:08:33 +0400

    task8

---
+++ Programson.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+
+
+            //string val;
+            //Console.WriteLine("deyeri daxil et");
+            //val = Console.ReadLine();
+            //char[] charArr = val.ToCharArray();
+            //for (int i = 0; i <charArr.Length; i++)
+            //{
+            //    if (char.IsDigit(charArr[i]))
+            //    {
+            //        Console.WriteLine("var");
+            //        Environment.Exit(0);
+            //    }
+            //}
+            //Console.WriteLine("yoxdur");
+            #endregion
+
+            #region task2
+
+
+            //string val;
+            //Console.WriteLine("deyeri daxil et");
+            //val = Console.ReadLine();
+            //char[] charArr = val.ToCharArray();
+            //int aExist = 0;
+            //int lExist = 0;
+            //for (int i = 0; i <charArr.Length; i++)
+            //{
+            //    // bir evvelki herf a dirsa bura isliyir
+            //    if (aExist == 1)
+            //    {
+            //        // bu herf l dirse lExsit 1 ele, dovru sonlandir deyilse bir aExist = 0; olsun yene
+            //        if (charArr[i] == 'l')
+            //        {
+            //            lExist = 1;
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            aExist = 0;
+            //        } // bura qederki qisim bir evvelki herf a dirsa isliyir deyilse aExist yeniden 0 eliyir
+
+            //    }
+
+
+            //    // burdan asagdaki qisim ise herf a dirsa aexisti 1 eliyir
+            //    if (charArr[i] == 'a')
+            //    {
+            //        aExist = 1;
+            //    }
+            //}
+            //if (aExist == 1 && lExist == 1)
+            //{
+            //    Console.WriteLine("a ve l ardicil var");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("a ve l ardicil yoxdur");
+            //}
+
+            #endregion
+
+
+            #region task4
+
+
+            //string[] words = new string[5];
+
+            //Console.WriteLine("5 soz girin");
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    words[i] = Console.ReadLine();
+            //}
+
+            //int counter = 0;
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    char[] charArr = words[i].ToCharArray();
+            //    for (int j = 0; j < charArr.Length; j++)
+            //    {
+            //        if (char.IsDigit(charArr[j]))
+            //        {
+            //            counter++;
+            //            break;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine("Say: " + counter);
+            #endregion
+
+
+            #region task3
+
+            
+        //    Console.WriteLine("eded daxil et");
+        //    Console.ReadLine();
+        //    string numberstr = Console.ReadLine();
+          
+        //        int s = 0;
+        //        int n = a[i];
+        //        while (n>0)
+        //        {
+        //       	int p = n % 10; 
+        //        s = s + p;
+        //        n = n / 10;
+
+        //        }
+        //        count++;
+        //        avg = s / count;
+        //    }
+        //    Console.WriteLine( "avg");
+
+        //}
+    #endregion
+
